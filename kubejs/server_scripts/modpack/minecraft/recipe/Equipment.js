85b959fb389bf2625906ee3c84da859a4d491d55
Forget_Melody <2734970539@qq.com>
2025-01-06 18:33:27 +0800

    配方

--- kubejs/server_scripts/modpack/minecraft/recipe/Equipment.js
+++ kubejs/server_scripts/modpack/minecraft/recipe/Equipment.js
@@ -4,7 +4,7 @@
 
 // 删除工具配方
 ServerEvents.recipes(event => {
-  const Traits = ["stone", "iron", "golden", "diamond", "netherite"];
+  const Traits = ["iron", "golden", "diamond", "netherite"];
   const Tools = ["axe", "hoe", "pickaxe", "shovel", "sword"];
   const BiFuncId = (traits, tool) => `minecraft:${traits}_${tool}`;
 
@@ -67,6 +67,36 @@ ServerEvents.recipes(event => {
     { "id": "minecraft:crossbow" },
     "*",
     Item.of('tconstruct:crossbow', '{Damage:0,tic_broken:0b,tic_materials:["tconstruct:wood","tconstruct:wood","tconstruct:string"],tic_modifiers:[{level:2,name:"tconstruct:cultivated"},{level:1,name:"tconstruct:stringy"}],tic_multipliers:{"tconstruct:durability":2.0f},tic_persistent:{},tic_stats:{"tconstruct:durability":120.0f},tic_volatile_data:{abilities:1,upgrades:3}}')
+  );
+  // 石斧 -> 石短柄斧
+  event.replaceOutput(
+    { id: "quark:tweaks/crafting/utility/tools/stone_axe" },
+    "*",
+    Item.of('tconstruct:hand_axe', '{Damage:0,tic_broken:0b,tic_materials:["tconstruct:rock","tconstruct:wood","tconstruct:wood"],tic_modifiers:[{level:1,name:"tconstruct:stonebound"},{level:2,name:"tconstruct:cultivated"},{level:1,name:"tconstruct:stripping"}],tic_persistent:{},tic_stats:{"tconstruct:attack_damage":7.0f,"tconstruct:attack_speed":0.9f,"tconstruct:durability":130.0f,"tconstruct:harvest_tier":"minecraft:stone","tconstruct:mining_speed":4.0f},tic_volatile_data:{abilities:1,upgrades:3}}')
+  );
+  // 石镐 -> 石镐
+  event.replaceOutput(
+    { id: "quark:tweaks/crafting/utility/tools/stone_pickaxe" },
+    "*",
+    Item.of('tconstruct:pickaxe', '{Damage:0,tic_broken:0b,tic_materials:["tconstruct:rock","tconstruct:wood","tconstruct:wood"],tic_modifiers:[{level:1,name:"tconstruct:stonebound"},{level:2,name:"tconstruct:cultivated"},{level:1,name:"tconstruct:pierce"}],tic_persistent:{},tic_stats:{"tconstruct:attack_damage":2.0f,"tconstruct:attack_speed":1.2f,"tconstruct:durability":130.0f,"tconstruct:harvest_tier":"minecraft:stone","tconstruct:mining_speed":4.0f},tic_volatile_data:{abilities:1,upgrades:3}}')
+  );
+  // 石镰 -> 石短刃镰
+  event.replaceOutput(
+    { id: "quark:tweaks/crafting/utility/tools/stone_hoe" },
+    "*",
+    Item.of('tconstruct:kama', '{Damage:0,tic_broken:0b,tic_materials:["tconstruct:rock","tconstruct:wood","tconstruct:wood"],tic_modifiers:[{level:1,name:"tconstruct:stonebound"},{level:2,name:"tconstruct:cultivated"},{level:1,name:"tconstruct:tilling"},{level:1,name:"tconstruct:shears"},{level:1,name:"tconstruct:harvest"}],tic_multipliers:{"tconstruct:attack_damage":0.5f},tic_persistent:{},tic_stats:{"tconstruct:attack_damage":1.0f,"tconstruct:attack_speed":1.6f,"tconstruct:durability":130.0f,"tconstruct:harvest_tier":"minecraft:stone","tconstruct:mining_speed":4.0f},tic_volatile_data:{abilities:1,upgrades:3}}')
+  );
+  // 石锹 -> 石凿石锹
+  event.replaceOutput(
+    { id: "quark:tweaks/crafting/utility/tools/stone_shovel" },
+    "*",
+    Item.of('tconstruct:pickadze', '{Damage:0,tic_broken:0b,tic_materials:["tconstruct:rock","tconstruct:wood","tconstruct:wood"],tic_modifiers:[{level:1,name:"tconstruct:stonebound"},{level:2,name:"tconstruct:cultivated"},{level:1,name:"tconstruct:pathing"}],tic_multipliers:{"tconstruct:attack_damage":1.15f,"tconstruct:durability":1.3f,"tconstruct:mining_speed":0.75f},tic_persistent:{},tic_stats:{"tconstruct:attack_damage":1.15f,"tconstruct:attack_speed":1.3f,"tconstruct:durability":123.49999f,"tconstruct:harvest_tier":"minecraft:stone","tconstruct:mining_speed":2.25f},tic_volatile_data:{abilities:1,upgrades:3}}')
+  );
+  // 石剑 -> 石剑 
+  event.replaceOutput(
+    { id: "quark:tweaks/crafting/utility/tools/stone_sword" },
+    "*",
+    Item.of('tconstruct:sword', '{Damage:0,tic_broken:0b,tic_materials:["tconstruct:rock","tconstruct:wood","tconstruct:wood"],tic_modifiers:[{level:1,name:"tconstruct:stonebound"},{level:2,name:"tconstruct:cultivated"},{level:1,name:"tconstruct:silky_shears"}],tic_multipliers:{"tconstruct:durability":1.1f,"tconstruct:mining_speed":0.5f},tic_persistent:{},tic_stats:{"tconstruct:attack_damage":4.0f,"tconstruct:attack_speed":1.6f,"tconstruct:durability":143.0f,"tconstruct:harvest_tier":"minecraft:stone","tconstruct:mining_speed":2.0f},tic_volatile_data:{abilities:1,upgrades:3}}')
   );
 })
 
